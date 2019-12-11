829255a762f68f5af784d3fa97fb3ba42cc9f0ca
聂祥 <1006556989@qq.com>
2019-12-11 19:45:27 +0800

    10分钟内不会重复提醒

--- sendmail/src/main/java/com/niex/sendmail/schedule/MonitorSchedule.java
+++ sendmail/src/main/java/com/niex/sendmail/schedule/MonitorSchedule.java
@@ -10,8 +10,12 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 
 import javax.mail.MessagingException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
 import java.util.Objects;
 
 /**
@@ -28,33 +32,48 @@ public class MonitorSchedule implements InitializingBean {
 
     private static Integer count = 0;
 
+    private static Long time = 0L;
+
 
     @Override
     public void afterPropertiesSet() throws Exception {
-        OhMyEmail.config(OhMyEmail.SMTP_QQ(true), "*", "*");
+        OhMyEmail.config(OhMyEmail.SMTP_QQ(true), "1006556989@qq.com", "123456789*");
     }
 
     @Scheduled(fixedRate = 60*1000)
     public void faceId() throws MessagingException {
-        try {
-            LOGGER.info("跑批开始-------------------------");
-            String s = OkHttp3Util.syncGet("http://192.168.151.67:9810/health");
-            LOGGER.info("响应信息：{}",s);
+        LOGGER.info("跑批开始-------------------------");
+        String s = OkHttp3Util.syncGet("http://192.168.151.67:9813/health");
+        LOGGER.info("响应信息：{}",s );
+        if (!StringUtils.isEmpty(s)) {
             JSONObject jsonObject = JSON.parseObject(s);
             String status = jsonObject.getString("status");
-            if (Objects.equals(status,"UP") || count > 1) {
+            if (Objects.equals(status,"UP")) {
                 return;
             }
+        }
+        if (count != 0 && System.currentTimeMillis() - time >= 600000) {
+            count = 0;
+        }
+        if (count == 0){
+            send();
+        }
+    }
+
+
+    public void send(){
+        try {
             OhMyEmail.subject("人脸项目服务器挂了！！！")
                     .from("监控邮箱")
-                    .to("*@163.com")
+                    .to("m18137666880@163.com")
                     .text("快去重启吧")
                     .send();
-            count++;
         } catch (SendMailException e) {
             e.printStackTrace();
         }
-
+        count++;
+        time = System.currentTimeMillis();
     }
+
 
 }
